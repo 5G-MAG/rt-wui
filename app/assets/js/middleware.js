109e4da79685edeb8bc6cf4d6c7ccc5007cb1920
Klaus Kuehnhammer <klaus@bitstem.com>
2021-10-19 08:58:42 +0200

    More name changes and refactoring: implemented comments from dsilhavy's review in PR2

--- app/assets/js/middleware.js
+++ app/assets/js/middleware.js
@@ -6,11 +6,11 @@ function human_file_size(size) {
 function poll(){
   $.get("/api/mw/files")
     .done( function(data, textStatus, xhr){
-      $("#gateway-running").show();
-      $("#gateway-not-running").hide();
+      $("#middleware-running").show();
+      $("#middleware-not-running").hide();
       let files = JSON.parse(data);
       files.sort((a, b) => (a.age > b.age) ? 1 : -1)
-      let tb = $("#gw-files-tbody");
+      let tb = $("#mw-files-tbody");
       tb.empty();
       let total_size = 0;
       for(let file of files) {
@@ -26,13 +26,13 @@ function poll(){
       $("#total-cache-size").text(human_file_size(total_size) + " total");
 
       $.get("/api/mw/services", function(data){
-        if (window.gw_services && window.gw_services == data) {
+        if (window.mw_services && window.mw_services == data) {
           return;
         }
-        window.gw_services = data;
+        window.mw_services = data;
         let services = JSON.parse(data);
         console.log(services);
-        let cont = $("#gw-services");
+        let cont = $("#mw-services");
         cont.empty();
         for(let service of services) {
           console.log(service);
@@ -70,13 +70,13 @@ function poll(){
       });
     })
     .fail( function(data, textStatus, xhr){
-      $("#gateway-running").hide();
-      $("#gateway-not-running").show();
+      $("#middleware-running").hide();
+      $("#middleware-not-running").show();
     });
 }
 
 $(function() {
-  $("#gateway-running").hide();
+  $("#middleware-running").hide();
   window.selected_mch = 0;
   setInterval(poll, 300);
 });
