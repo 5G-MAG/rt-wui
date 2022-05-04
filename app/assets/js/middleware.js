496f4db60fa4410ede15764cc67394322cad89b9
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2022-05-04 15:34:12 +0200

    Provide HLS manifest url directly to application.js if available (#8)

--- app/assets/js/middleware.js
+++ app/assets/js/middleware.js
@@ -1,11 +1,11 @@
 function human_file_size(size) {
-    var i = Math.floor( Math.log(size) / Math.log(1024) );
-    return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+  var i = Math.floor(Math.log(size) / Math.log(1024));
+  return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
 };
 
-function poll(){
+function poll() {
   $.get("/api/mw/files")
-    .done( function(data, textStatus, xhr){
+    .done(function (data, textStatus, xhr) {
       $("#middleware-running").show();
       $("#middleware-not-running").hide();
       let files = JSON.parse(data);
@@ -14,25 +14,28 @@ function poll(){
       tb.empty();
       let total_size = 0;
       let dashManifestUrl = null;
-      for(let file of files) {
+      let hlsManifestUrl = null;
+      for (let file of files) {
         let row = $("<tr>");
         row.append($("<td>").text(file.age));
         row.append($("<td>").text(file.tmgi));
         let requiredSlash = file.location.charAt(0) === '/' ? '' : '/';
-        let url = '/f/' + file.tmgi + requiredSlash + file.location ;
+        let url = '/f/' + file.tmgi + requiredSlash + file.location;
         row.append($("<td>").html("<a target='_blank' href=" + url + ">" + file.location + "</a>"));
         row.append($("<td>").text(human_file_size(file.content_length)));
         row.append($("<td>").text(file.access_count));
         tb.append(row);
         total_size += file.content_length;
 
-        if(file.location && file.location.indexOf(".mpd") !== -1) {
-            dashManifestUrl = url;
+        if (file.location && file.location.indexOf(".mpd") !== -1) {
+          dashManifestUrl = url;
+        } else if (file.location && file.location.indexOf("index.m3u8") !== -1) {
+          hlsManifestUrl = url;
         }
       }
       $("#total-cache-size").text(human_file_size(total_size) + " total");
 
-      $.get("/api/mw/services", function(data){
+      $.get("/api/mw/services", function (data) {
         if (window.mw_services && window.mw_services == data) {
           return;
         }
@@ -41,7 +44,7 @@ function poll(){
         console.log(services);
         let cont = $("#mw-services");
         cont.empty();
-        for(let service of services) {
+        for (let service of services) {
           console.log(service);
           let row = $("<div class='row m-3'>");
           let col = $("<div class='col-lg-12'>");
@@ -56,18 +59,21 @@ function poll(){
           p = $("<p class='mb-0'>").html("Stream multicast: <strong>" + service.stream_mcast + "</strong>");
           box.append(p);
           let url = "";
-          if (service.stream_type=="FLUTE/UDP") {
-            url = "/application?s="+service.stream_tmgi;
-            if(dashManifestUrl) {
-                url += "&m=" + dashManifestUrl + "&p=dash";
+          if (service.stream_type == "FLUTE/UDP") {
+            url = "/application?s=" + service.stream_tmgi;
+            if (dashManifestUrl) {
+              url += "&m=" + dashManifestUrl + "&p=dash";
+            }
+            else if (hlsManifestUrl) {
+              url += "&m=" + hlsManifestUrl + "&p=hls";
             }
             let stmgi = parseInt(service.stream_tmgi, 16);
             p = $("<p class='mb-0'>").html("Stream TMGI: <strong>0x" + stmgi.toString(16) + "</strong>");
             box.append(p);
           } else {
-            url = "udp://@"+service.stream_mcast;
+            url = "udp://@" + service.stream_mcast;
           }
-          let pa = $("<a href='"+url+"'>");
+          let pa = $("<a href='" + url + "'>");
           let pb = $("<button class='bg-dark play-button'>").html("▷");
           pa.append(pb);
           box.append(pa);
@@ -79,13 +85,13 @@ function poll(){
         }
       });
     })
-    .fail( function(data, textStatus, xhr){
+    .fail(function (data, textStatus, xhr) {
       $("#middleware-running").hide();
       $("#middleware-not-running").show();
     });
 }
 
-$(function() {
+$(function () {
   $("#middleware-running").hide();
   window.selected_mch = 0;
   setInterval(poll, 300);
